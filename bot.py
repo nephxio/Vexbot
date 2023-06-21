a54be382bd551db4f9092d92cd3649f10caf6fab
Phil <phil.wallace83@gmail.com>
2023-06-21 12:37:44 -0400

    Basic login flow, added .env to gitignore

--- bot.py
+++ bot.py
@@ -1,9 +1,23 @@
+import os  # for importing env vars for the bot to use
+from twitchio.ext import commands
+from dotenv import load_dotenv
+
+load_dotenv()
 
 
-def main():
-    pass
+class Bot(commands.Bot):
+
+    def __init__(self):
+        super().__init__(token=os.getenv("TMI_TOKEN"),
+                         prefix=os.getenv("BOT_PREFIX"),
+                         initial_channels=[os.getenv("CHANNEL")])
+
+    async def event_ready(self):
+        # Log to console when bot is ready
+        print(f"Logged in as | {self.nick}")
+        print(f"User id is | {self.user_id}")
+        print(f"Room is | {self.connected_channels}")
 
 
-if __name__ == '__bot__':
-    main()
-
+bot = Bot()
+bot.run()

17961e999dd4219c0e6373f14b15a424c296cf23
Phil <phil.wallace83@gmail.com>
2023-06-21 15:07:15 -0400

    Functionality for Tasks and Tasklists

---
+++ task.py
@@ -0,0 +1,33 @@
+import datetime
+
+
+class Task:
+
+    def __init__(self, owner_id: int, owner_nick: str, task_name: str, task_desc: str = "",
+                 is_current_task: bool = True, is_complete: bool = False, is_deleted: bool = False,
+                 timestamp: datetime.datetime = datetime.datetime.now()) -> None:
+        self.owner_id: int = owner_id
+        self.owner_nick: str = owner_nick
+        self.task_name: str = task_name
+        self.task_desc: str = task_desc
+        self.is_current_task: bool = is_current_task
+        self.is_complete: bool = is_complete
+        self.is_deleted: bool = is_deleted
+        self.timestamp: datetime.datetime = timestamp
+
+    def get_elapsed_time(self) -> str:
+        elapsed_time: datetime.timedelta = datetime.datetime.now() - self.timestamp
+
+        return str(datetime.timedelta(seconds=elapsed_time.total_seconds()))
+
+    def edit_task_desc(self, new_desc: str) -> None:
+        self.task_desc = new_desc
+
+    def mark_as_done(self) -> None:
+        self.is_complete = True
+
+    def delete_task(self) -> None:
+        self.is_deleted = True
+
+    def get_current_task(self) -> str:
+        return f"Current task for {self.owner_nick} : {self.task_name} - {self.task_desc}"
---
+++ tasklist.py
@@ -0,0 +1,72 @@
+import twitchio
+from task import Task
+from typing import List
+
+
+class TaskList:
+
+    def __init__(self):
+        self.task_list = []
+
+    def task_exists(self, new_task: Task) -> bool:
+        for task in self.task_list:
+            if (task.owner_id == new_task.owner_id) and (task.task_name == new_task.task_name):
+                return True
+        return False
+
+    def has_active_task(self, user: twitchio.PartialUser) -> bool:
+        for task in self.task_list:
+            if (task.owner_id == user.id) and task.is_current_task:
+                return True
+        return False
+
+    def add_task(self, new_task: Task) -> None:
+        if not self.task_exists(new_task):
+            self.task_list.append(new_task)
+
+    def edit_current_task_desc(self, user: twitchio.PartialUser, new_desc: str) -> bool:
+        if self.has_active_task(user.id):
+            for task in self.task_list:
+                if (task.owner_id == user.id) and task.is_current_task:
+                    task.task_desc = new_desc
+                    return True
+        return False
+
+    def delete_active_task(self, user: twitchio.PartialUser) -> bool:
+        if self.has_active_task(user.id):
+            for task in self.task_list:
+                if (task.owner_id == user.id) and task.is_current_task:
+                    task.is_deleted = True
+                    task.is_current_task = False
+                    return True
+        return False
+
+    def mark_task_done(self, user: twitchio.PartialUser) -> bool:
+        if self.has_active_task(user.id):
+            for task in self.task_list:
+                if (task.owner_id == user.id) and task.is_current_task:
+                    task.is_complete = True
+                    task.is_current_task = False
+                    return True
+        return False
+
+    def get_current_task(self, user: twitchio.PartialUser) -> str:
+        if self.has_active_task(user.id):
+            for task in self.task_list:
+                if (task.owner_id == user.id) and task.is_current_task:
+                    return task.get_current_task()
+        return f"No active tasks found for {user.name}."
+
+    def get_accumulated_time(self, user: twitchio.PartialUser) -> str:
+        if self.has_active_task(user.id):
+            for task in self.task_list:
+                if (task.owner_id == user.id) and task.is_current_task:
+                    return f"{user.name}, you have been working on your current task for: {task.get_elapsed_time()}"
+        return f"{user.name}, you have no active tasks."
+
+    def list_tasks(self, user: twitchio.PartialUser) -> List[Task]:
+        completed_tasks = []
+        for task in self.task_list:
+            if (task.owner_id == user.id) and task.is_complete:
+                completed_tasks.append(task)
+        return completed_tasks
